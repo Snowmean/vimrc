f9eff72373783aac43f298cdd54c36f84744388c
Pavel Pivovarov <ppivovarov@switch.tv>
2016-02-11 15:54:07 +1100

    Fix vimrc URL

--- vimrc.sh
+++ vimrc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Downloading VIM config file
-curl -fLo ~/.vimrc --create-dirs https://raw.githubusercontent.com/snowmean/vimrc/vimrc
+curl -fLo ~/.vimrc --create-dirs https://raw.githubusercontent.com/snowmean/vimrc/master/vimrc
 
 # Downloading and installing vim plugin manager
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
